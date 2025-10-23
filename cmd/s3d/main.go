8c32f497e9449c75a9cb3696405cddae859d1ccf
Chris Schinnerl <3903476+ChrisSchinnerl@users.noreply.github.com>
2025-10-23 16:35:12 +0200

    Configure tox access key

--- cmd/s3d/main.go
+++ cmd/s3d/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 
@@ -9,12 +10,22 @@ import (
 	"go.uber.org/zap"
 )
 
+const (
+	toxAccessKey = "0555b35654ad1656d804"
+	toxSecretKey = "h7GhxuBLTrlhVUyxSPUKUV8r/2EI4ngqJxD7iBdBYLhwluN30JaT3Q=="
+)
+
 func main() {
 	logger, err := zap.NewDevelopment()
 	if err != nil {
 		log.Fatalf("failed to create logger: %v", err)
 	}
-	s3 := s3.New(testutils.NewMemoryBackend(),
+	backend := testutils.NewMemoryBackend()
+	err = backend.AddAccessKey(context.Background(), toxAccessKey, toxSecretKey)
+	if err != nil {
+		log.Fatalf("failed to add access key: %v", err)
+	}
+	s3 := s3.New(backend,
 		s3.WithHostBucketBases([]string{"localhost"}),
 		s3.WithLogger(logger))
 	http.ListenAndServe("localhost:7777", s3)
