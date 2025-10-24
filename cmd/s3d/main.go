8f0c7de369eb72a4f625bb3b53f9d6c19c4a0225
Chris Schinnerl <3903476+ChrisSchinnerl@users.noreply.github.com>
2025-10-24 11:33:08 +0200

    add missing keys

--- cmd/s3d/main.go
+++ cmd/s3d/main.go
@@ -13,6 +13,23 @@ import (
 const (
 	toxAccessKey = "0555b35654ad1656d804"
 	toxSecretKey = "h7GhxuBLTrlhVUyxSPUKUV8r/2EI4ngqJxD7iBdBYLhwluN30JaT3Q=="
+
+	altAccessKey = "NOPQRSTUVWXYZABCDEFG"
+	altSecretKey = "nopqrstuvwxyzabcdefghijklmnabcdefghijklm"
+
+	tenantAccessKey = "HIJKLMNOPQRSTUVWXYZA"
+	tenantSecretKey = "opqrstuvwxyzabcdefghijklmnopqrstuvwxyzab"
+)
+
+var (
+	toxKeyPairs = []struct {
+		AccessKey string
+		SecretKey string
+	}{
+		{toxAccessKey, toxSecretKey},
+		{altAccessKey, altSecretKey},
+		{tenantAccessKey, tenantSecretKey},
+	}
 )
 
 func main() {
@@ -21,10 +38,14 @@ func main() {
 		log.Fatalf("failed to create logger: %v", err)
 	}
 	backend := testutils.NewMemoryBackend()
-	err = backend.AddAccessKey(context.Background(), toxAccessKey, toxSecretKey)
-	if err != nil {
-		log.Fatalf("failed to add access key: %v", err)
+
+	for _, pair := range toxKeyPairs {
+		err = backend.AddAccessKey(context.Background(), pair.AccessKey, pair.SecretKey)
+		if err != nil {
+			log.Fatalf("failed to add access key: %v", err)
+		}
 	}
+
 	s3 := s3.New(backend,
 		s3.WithHostBucketBases([]string{"localhost"}),
 		s3.WithLogger(logger))
