ead56d0aa7f3547a53951f2bffacd6f17717870d
Chris Schinnerl <3903476+ChrisSchinnerl@users.noreply.github.com>
2025-10-30 14:37:07 +0100

    move s3d to s3d-tox

--- cmd/s3d/main.go
+++ cmd/s3d/main.go
@@ -1,55 +1,5 @@
 package main
 
-import (
-	"context"
-	"log"
-	"net/http"
-
-	"github.com/SiaFoundation/s3d/s3"
-	"github.com/SiaFoundation/s3d/testutils"
-	"go.uber.org/zap"
-)
-
-const (
-	toxAccessKey = "0555b35654ad1656d804"
-	toxSecretKey = "h7GhxuBLTrlhVUyxSPUKUV8r/2EI4ngqJxD7iBdBYLhwluN30JaT3Q=="
-
-	altAccessKey = "NOPQRSTUVWXYZABCDEFG"
-	altSecretKey = "nopqrstuvwxyzabcdefghijklmnabcdefghijklm"
-
-	tenantAccessKey = "HIJKLMNOPQRSTUVWXYZA"
-	tenantSecretKey = "opqrstuvwxyzabcdefghijklmnopqrstuvwxyzab"
-)
-
-var (
-	toxKeyPairs = []struct {
-		AccessKey string
-		SecretKey string
-	}{
-		{toxAccessKey, toxSecretKey},
-		{altAccessKey, altSecretKey},
-		{tenantAccessKey, tenantSecretKey},
-	}
-)
-
 func main() {
-	logger, err := zap.NewDevelopment()
-	if err != nil {
-		log.Fatalf("failed to create logger: %v", err)
-	}
-	backend := testutils.NewMemoryBackend()
-
-	for _, pair := range toxKeyPairs {
-		err = backend.AddAccessKey(context.Background(), pair.AccessKey, pair.SecretKey)
-		if err != nil {
-			log.Fatalf("failed to add access key: %v", err)
-		}
-	}
-
-	s3 := s3.New(backend,
-		s3.WithHostBucketBases([]string{"localhost"}),
-		s3.WithLogger(logger))
-	if err := http.ListenAndServe("localhost:8000", s3); err != nil {
-		log.Printf("failed to start server: %v", err)
-	}
+	// TODO: implement
 }
