883bef20488c009ee8d4362b97f29529b05b05b0
Chris Schinnerl <3903476+ChrisSchinnerl@users.noreply.github.com>
2025-11-10 16:21:28 +0100

    run v2 object listing compat tests

--- s3/objects.go
+++ s3/objects.go
@@ -296,17 +296,27 @@ type ObjectsListResult struct {
 	// This is a convenience for backend implementers like s3bolt and s3mem,
 	// which operate on a full, flat list of keys.
 	prefixes map[string]bool
+	maxKeys  int64
 }
 
 // NewObjectsListResult creates a new, empty ObjectsListResult. Use Add and
 // AddPrefix to populate it.
-func NewObjectsListResult() *ObjectsListResult {
-	return &ObjectsListResult{}
+func NewObjectsListResult(maxKeys int64) *ObjectsListResult {
+	return &ObjectsListResult{maxKeys: maxKeys}
 }
 
 // Add adds an object to the result.
 func (b *ObjectsListResult) Add(item *Content) {
-	b.Contents = append(b.Contents, item)
+	if len(b.Contents)+len(b.CommonPrefixes) < int(b.maxKeys) {
+		b.Contents = append(b.Contents, item)
+	}
+	if len(b.Contents)+len(b.CommonPrefixes) >= int(b.maxKeys) {
+		if b.NextMarker == "" {
+			b.NextMarker = item.Key
+		} else {
+			b.IsTruncated = true
+		}
+	}
 }
 
 // AddPrefix adds a common prefix to the result. If the prefix has already been
@@ -317,8 +327,17 @@ func (b *ObjectsListResult) AddPrefix(prefix string) {
 	} else if b.prefixes[prefix] {
 		return
 	}
-	b.prefixes[prefix] = true
-	b.CommonPrefixes = append(b.CommonPrefixes, CommonPrefix{Prefix: prefix})
+	if len(b.Contents)+len(b.CommonPrefixes) < int(b.maxKeys) {
+		b.prefixes[prefix] = true
+		b.CommonPrefixes = append(b.CommonPrefixes, CommonPrefix{Prefix: prefix})
+	}
+	if len(b.Contents)+len(b.CommonPrefixes) >= int(b.maxKeys) {
+		if b.NextMarker == "" {
+			b.NextMarker = prefix
+		} else {
+			b.IsTruncated = true
+		}
+	}
 }
 
 func (s *s3) listObjectsV2(w http.ResponseWriter, r *http.Request, accessKeyID *string, bucket string) error {
@@ -337,6 +356,16 @@ func (s *s3) listObjectsV2(w http.ResponseWriter, r *http.Request, accessKeyID *
 	objects, err := s.backend.ListObjects(r.Context(), accessKeyID, bucket, prefix, page)
 	if err != nil {
 		return err
+	}
+
+	// URL-escape object keys and common prefixes if requested
+	if r.FormValue("encoding-type") == "url" {
+		for i := range objects.Contents {
+			objects.Contents[i].Key = urlEscape(objects.Contents[i].Key)
+		}
+		for i := range objects.CommonPrefixes {
+			objects.CommonPrefixes[i].Prefix = urlEscape(objects.CommonPrefixes[i].Prefix)
+		}
 	}
 
 	// prepare result
