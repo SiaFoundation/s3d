df301b22f1ca45443ed760fcb11ae0f2c4d56547
PJ <peterjan.brone@gmail.com>
2026-05-07 09:46:28 +0200

    sia: use max instead of min

--- sia/objects.go
+++ sia/objects.go
@@ -227,7 +227,7 @@ func (s *Sia) headOrGetObject(ctx context.Context, accessKeyID *string, bucket, 
 
 	var resp *s3.Object
 	if partNumber != nil {
-		partsCount := min(obj.PartsCount, 1)
+		partsCount := max(obj.PartsCount, 1)
 		resp = &s3.Object{
 			Body:         nil,
 			ContentMD5:   obj.ContentMD5,
