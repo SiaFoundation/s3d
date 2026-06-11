d09e7b237912f14ae7b9f319b99392df0fca6f30
PJ <peterjan.brone@gmail.com>
2026-06-11 09:56:44 +0200

    all: address comments

--- s3/s3.go
+++ s3/s3.go
@@ -321,8 +321,8 @@ func corsMiddleware(handler http.Handler) http.Handler {
 }
 
 // NewAdmin creates an HTTP handler that serves the admin API using the provided
-// backend. Currently the only endpoint is /prometheus, which exposes the
-// background upload stats as Prometheus metrics.
+// backend. It exposes /prometheus, which serves the background upload stats as
+// Prometheus metrics, and /upload/stats, which serves the same stats as JSON.
 func NewAdmin(b Backend, opts ...Option) http.Handler {
 	s3 := &s3{
 		backend: b,
