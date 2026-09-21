1b3878a5574a6a9e5c9b06f43f4e04707ba5ca3b
Chris Schinnerl <3903476+ChrisSchinnerl@users.noreply.github.com>
2026-09-21 15:41:09 +0200

    best effort remove dir when failing to sync it

--- sia/multipart.go
+++ sia/multipart.go
@@ -39,6 +39,7 @@ func (s *Sia) createMultipartUploadDir(uploadID string) (string, error) {
 	if err := os.Mkdir(uploadDir, 0700); err != nil {
 		return "", fmt.Errorf("failed to create upload directory: %w", err)
 	} else if err := syncDir(s.uploadDir()); err != nil {
+		_ = os.RemoveAll(uploadDir)
 		return "", fmt.Errorf("failed to sync upload directory: %w", err)
 	}
 	return uploadDir, nil
@@ -50,9 +51,7 @@ func (s *Sia) multipartPartPath(uploadID s3.UploadID, partNumber int) string {
 
 func (s *Sia) ensureMultipartPartDir(uploadID s3.UploadID, partNumber int) (string, error) {
 	partDir := s.multipartPartPath(uploadID, partNumber)
-	if err := os.Mkdir(partDir, 0700); os.IsExist(err) {
-		return partDir, nil
-	} else if err != nil {
+	if err := os.Mkdir(partDir, 0700); err != nil && !os.IsExist(err) {
 		return "", fmt.Errorf("failed to create part directory: %w", err)
 	} else if err := syncDir(s.multipartUploadPath(uploadID.String())); err != nil {
 		return "", fmt.Errorf("failed to sync upload directory: %w", err)
